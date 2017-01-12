194eb99d613a942e79bb71aa169f47d51c132786
Krzysztof Szynter <kszynter@gmail.com>
2017-01-12 17:08:53 +0100

    added handling other errors

--- __init__.py
+++ __init__.py
@@ -17,6 +17,14 @@ def dashboard():
     return render_template("dashboard.html", TOPIC_DICT=TOPIC_DICT)
 
 
+@app.route('/slashboard/')
+def slashboard():
+    try:
+        return render_template("dashboard.html", TOPIC_DICT=ssas)
+    except Exception as e:
+        return render_template("500.html", error=e)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html")
